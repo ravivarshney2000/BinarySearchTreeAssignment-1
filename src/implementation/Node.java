db63252f28a3ba9ae12225ddfbde4700d25f89b8
ravivarshney2000 <67819829+ravivarshney2000@users.noreply.github.com>
2021-04-13 15:21:34 +0530

    Update Node.java

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -1,5 +1,14 @@
 package implementation;
 
 public class Node<E extends Comparable<E>> implements myinterface.Node<E> {
+    private E data;
+    private Node<E> left;
+    private Node<E> right;
+
+    public Node(E data){
+        this.data=data;
+        left=null;
+        right=null;
+    }
     //complete node class
 }
