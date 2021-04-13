b586bce3aa428b15e31daec54ea6e3ddb47d60e6
ravivarshney2000 <67819829+ravivarshney2000@users.noreply.github.com>
2021-04-13 15:30:43 +0530

    Update MyBinarySearchTree.java

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -117,6 +117,11 @@ public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearch
 
     @Override
     public int height(Node<E> node) {
-        return 0;
+        if (node==null){
+            return -1;
+        }
+        else {
+            return 1+Math.max(height(node.left()),height(node.right()));
+        }
     }
 }
