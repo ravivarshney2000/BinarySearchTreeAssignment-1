69e38ec8ec7dcbaeab861f749bc1c6041c8b156e
ravivarshney2000 <67819829+ravivarshney2000@users.noreply.github.com>
2021-04-13 15:17:42 +0530

    Update MyBinarySearchTree.java

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -5,34 +5,108 @@ import myinterface.Node;
 
 public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearchTreeADT<E> {
     //complete this class
+    
+    private Node<E> root;
 
     @Override
     public void insert(E data) {
+        root = insert(root, data);
 
     }
+    
+    private Node<E> insert(Node<E> root, E data)
+	{
+		
+		if (root == null)
+		{
+			return new Node<E>(data);
+		}
+		
+		else if (data.compareTo(root.data) < 0)
+		{
+			root.left = insert(root.left, data);
+		}
+		
+		else 
+		{
+			root.right = insert(root.right, data);
+		}
+		
+
+		return root;
+	}
 
     @Override
     public boolean search(E searchElement) {
-        return false;
+        return search(root, data);
     }
+    
+    private boolean search(Node<E> root, E data)
+	{
+		
+		if (root == null)
+		{
+			return false;
+		}
+		
+		else if (data.compareTo(root.data) < 0)
+		{
+			return contains(root.left, data);
+		}
+		
+		else if (data.compareTo(root.data) > 0)
+		{
+			return contains(root.right, data);
+		}
+		
+		else
+		{
+			return true;
+		}
+	}
 
     @Override
     public void inOrder(Node<E> node) {
+        if (root == null)
+			return;
 
+		inorder(root.left);
+		System.out.print(" " + root.data);
+		inorder(root.right);
+     
     }
 
     @Override
     public void preOrder(Node<E> node) {
+        if (root == null)
+			return;
+
+        System.out.print(" " + root.data);
+		preorder(root.left);
+		preorder(root.right);
+        
 
     }
 
     @Override
     public void postOrder(Node<E> node) {
+        if (root == null)
+			return;
+
+		postorder(root.left);
+		postorder(root.right);
+		System.out.print(" " + root.data);
 
     }
 
     @Override
     public void reverseInOrder(Node<E> node) {
+        if (root == null)
+			return;
+
+		reverseInOrder(root.right);
+		System.out.print(" " + root.data);
+		reverseInOrder(root.left);
 
     }
 
